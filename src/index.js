058d5a8b4e76c86358bbd6d926e31c3f4f93401a
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-01-17 19:29:08 +0200

    Add resources and engine handlebars

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 5000;
+
+
+app.use('/static', express.static('public'));
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views')
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, () => console.log(`App is working on port ${port}...`));
