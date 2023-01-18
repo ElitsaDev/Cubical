fe8e2fd98b50407da34371a644a0297e6f7739cc
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-01-18 20:10:20 +0200

    Added view engine

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const config = require('../config/config')
 const routes = require('./routes');
+const config = require('../config/config');
+const setupViewEngine = require('../config/viewEngine');
+
 const app = express();
-const port = 5000;
+setupViewEngine(app);
 
 app.use('/static', express.static('public'));
 app.use(express.urlencoded({extended: false}));
-
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', './src/views')
 
 app.use(routes);
 
