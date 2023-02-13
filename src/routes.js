6829ade7b39b4020aee5b23252a82574e383dece
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-02-13 15:40:25 +0200

    add create accessory controller

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
+const accessoryController = require('./controllers/accessoryController');
 
 const router = express.Router();
 
 router.use('/', homeController);
 router.use('/cube', cubeController);
 router.use('/details/:cubeId', cubeController);
+router.use('/accessory', accessoryController);
 router.use('/404', homeController);
 
 module.exports = router;
